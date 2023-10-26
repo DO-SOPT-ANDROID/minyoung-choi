7ab8f18ac142bf40058e905225d97888d32b7108
codingmy <codingmy@naver.com>
2023-10-27 05:36:56 +0900

    [ui] 네비게이션바 위치하는 메인페이지 구성

---
+++ app/src/main/java/org/sopt/dosopttemplate/MainHomeActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.dosopttemplate
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainHomeActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainHomeBinding
+
+
+
+
+}
