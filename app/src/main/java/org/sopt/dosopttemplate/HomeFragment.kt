7386c8e5b31c35a1ccb95db80c03ae8199a36a7e
codingmy <codingmy@naver.com>
2023-10-27 17:29:31 +0900

    [feat] 세미나 내용 적용

---
+++ app/src/main/java/org/sopt/dosopttemplate/HomeFragment.kt
@@ -0,0 +1,47 @@
+package org.sopt.dosopttemplate
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+
+
+class HomeFragment :Fragment(){
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+    get() = requireNotNull(_binding) { "바인딩 객체가 생성되지 않았다. 생성하고 불러라 임마!" }
+    private val mockFriendList =listOf <Friend>(
+        Friend(
+            profileImage = R.drawable.pr_image,
+            name = "친구1",
+            self_description = "상태창1",
+        ),
+        Friend(
+            profileImage = R.drawable.pr_image,
+            name = "친구2",
+            self_description = "상태창2",
+        ),
+    )
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val friendAdapter = FriendAdapter(requireContext())
+        binding.rvFriend.adapter= friendAdapter
+        friendAdapter.setFriendList(mockFriendList)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/org/sopt/dosopttemplate/MainHomeActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/MainHomeActivity.kt
@@ -1,12 +1,57 @@
 package org.sopt.dosopttemplate
 
 import androidx.appcompat.app.AppCompatActivity
+import android.app.TaskStackBuilder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import org.sopt.dosopttemplate.databinding.ActivityMainhomeBinding
 
 
-class MainHomeActivity: AppCompatActivity() {
-    private lateinit var binding: ActivityMainHomeBinding
+class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainhomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainhomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_home)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fcv_home, HomeFragment())
+                .commit()
+        }
 
+        clickBottomNavigation()
+    }
 
+    private fun clickBottomNavigation() {
+        binding.bnvHome.setOnItemSelectedListener{
+            when (it.itemId) {
+                R.id.menu_home-> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
 
+                R.id.menu_do_android-> {
+                    replaceFragment(DoAndroidFragment())
+                    true
+                }
+
+                R.id.menu_mypage-> {
+                    replaceFragment(MyPageFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    // Activity에서 Fragment를 다뤄야 하니 supportFragmentManager를 사용합니다.
+// 트렌젝션을 시작하고 replace 메서드를 통해 Fragment를 교체합니다.
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_home, fragment)
+            .commit()
+    }
 }
