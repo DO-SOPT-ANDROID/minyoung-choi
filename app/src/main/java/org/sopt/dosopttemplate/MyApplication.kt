e16f793d1eb254fb7358fd9cf6be5faa4efdee8d
codingmy <codingmy@naver.com>
2023-11-06 02:40:46 +0900

    [feat] myApplication, preferenceUtil 클래스 추가

---
+++ app/src/main/java/org/sopt/dosopttemplate/MyApplication.kt
@@ -0,0 +1,17 @@
+package org.sopt.dosopttemplate
+
+import android.app.Application
+
+class MyApplication : Application()
+{
+    companion object
+    {
+        lateinit var prefs: PreferenceUtil
+    }
+
+    override fun onCreate()
+    {
+        prefs = PreferenceUtil(applicationContext)
+        super.onCreate()
+    }
+}
