2f78367533a11e0ab7af746e8288edc93f37632f
codingmy <codingmy@naver.com>
2023-11-17 06:16:57 +0900

    [feat] SignUpActivity에서 서버통신 통한 회원가입 구현

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 import com.google.android.material.snackbar.Snackbar
 import android.widget.Toast
+import org.sopt.dosopttemplate.ServicePool.authService
 //import com.codingmy.sopt_w1_hw1.databinding.ActivitySignupBinding
 import org.sopt.dosopttemplate.databinding.ActivitySignupBinding
+import org.sopt.dosopttemplate.retrofit2data.RequestLoginDto
+import org.sopt.dosopttemplate.retrofit2data.RequestSignUpDto
+import org.sopt.dosopttemplate.retrofit2data.ResponseLoginDto
+import org.sopt.dosopttemplate.retrofit2data.ResponseSignUpDto
+import retrofit2.Call
+import retrofit2.Response
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -26,13 +33,16 @@ class SignUpActivity : AppCompatActivity() {
             //가입 조건 확인
             if (checkCondition()) {
                 //토스트 띄우기
-                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
-
                 val intent = Intent(this, LoginActivity::class.java)
+                signUp()
+
+                Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show()
+/*
                 //유저정보 -> 리스트로 구성
                 val userInfoList = UserInfoToListString()
 
                 sendUserInfo(intent, userInfoList)
+*/
                 //액티비티 이동
                 startActivity(intent)
 
@@ -50,6 +60,7 @@ class SignUpActivity : AppCompatActivity() {
     private fun checkCondition() =
         (binding.etSignupId.text.length < 11 && binding.etSignupId.text.length > 5) && (binding.etSignupPw.text.length < 13 && binding.etSignupPw.text.length > 7) && (binding.etSignupNickname.text.length > 0 && !binding.etSignupNickname.text.isBlank())
 
+/*
     private fun sendUserInfo(
         intent: Intent,
         userInfoList: List<String>
@@ -58,7 +69,9 @@ class SignUpActivity : AppCompatActivity() {
         intent.putStringArrayListExtra("userInfoList", ArrayList(userInfoList))
         setResult(RESULT_OK, intent)
     }
+*/
 
+/*
     private fun UserInfoToListString(): List<String> {
         val userInfoList = listOf<String>(
             binding.etSignupId.text.toString(),
@@ -67,6 +80,34 @@ class SignUpActivity : AppCompatActivity() {
             binding.etSignupMbti.text.toString()
         )
         return userInfoList
+    }
+*/
+
+    private fun signUp() = with(binding) {
+        val id = etSignupId.text.toString();
+        val pw =etSignupPw.text.toString()
+        val nickname=etSignupNickname.text.toString()
+
+        btSignupButton.setOnClickListener {
+            authService.signUp(RequestSignUpDto(id, pw, nickname))
+                .enqueue(object : retrofit2.Callback<ResponseSignUpDto> {
+                        override fun onResponse(
+                        call: Call<ResponseSignUpDto>,
+                        response: Response<ResponseSignUpDto>,
+                    ) {
+                        if (response.isSuccessful) {
+                            Toast.makeText(
+                                this@SignUpActivity,
+                                "회원가입 성공",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                        Toast.makeText(this@SignUpActivity, "서버 에러 발생", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
     }
 }
 
