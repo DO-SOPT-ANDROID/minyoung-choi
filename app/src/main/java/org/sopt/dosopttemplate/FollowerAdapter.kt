fd053cde468409882e40ede45368e3df78f27226
codingmy <codingmy@naver.com>
2023-11-17 20:15:48 +0900

    [add] 리스트 생성 위한 data class Follower 추가

---
+++ app/src/main/java/org/sopt/dosopttemplate/FollowerAdapter.kt
@@ -0,0 +1,33 @@
+package org.sopt.dosopttemplate
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.dosopttemplate.databinding.ItemFollowerBinding
+
+class FollowerAdapter(context: Context) : RecyclerView.Adapter<FollowerViewHolder>() {
+    private val inflater by lazy {
+        LayoutInflater.from(context)
+    }
+
+    //로그인한 유저의 프로필 담을 곳 생성
+    private var FollowerInfo: List<Follower> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FollowerViewHolder {
+        val binding = ItemFollowerBinding.inflate(inflater, parent, false)
+        return FollowerViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FollowerViewHolder, position: Int) {
+        holder.onBind(FollowerInfo[position])
+    }
+
+    override fun getItemCount(): Int = FollowerInfo.size
+
+    fun setFollowerList(FollowerInfo: List<Follower>) {
+        this.FollowerInfo = FollowerInfo.toList()
+        notifyDataSetChanged()
+    }
+
+}
