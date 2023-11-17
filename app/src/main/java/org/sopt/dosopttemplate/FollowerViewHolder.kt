3703596bc5915c8868fcee537d8f57b86617e63b
codingmy <codingmy@naver.com>
2023-11-17 21:37:30 +0900

    [fix] 팔로워 프로필 로드 구현

--- app/src/main/java/org/sopt/dosopttemplate/FollowerViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/FollowerViewHolder.kt
@@ -3,14 +3,15 @@ package org.sopt.dosopttemplate
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.dosopttemplate.databinding.ItemFollowerBinding
 import org.sopt.dosopttemplate.follower.FollowerDto
+import coil.load
+import coil.transform.RoundedCornersTransformation
 
 class FollowerViewHolder (private val binding: ItemFollowerBinding) : RecyclerView.ViewHolder(binding.root){
 
         fun onBind(FollowerData: FollowerDto) =with(binding){
 //프사 이미지 로딩 손봐야됨
-            homeFollowerIvProfile.load(item.avatar) {
-            homeFollowerIvProfile.setImageResource(FollowerData.avatar)
-                ///
+            homeFollowerIvProfile.load(FollowerData.avatar) {
+                transformations(RoundedCornersTransformation(10.0F))}
             homeFollowerTvName.text = FollowerData.first_name+FollowerData.last_name
             homeFollowerTvUserMail.text=FollowerData.email
         }
