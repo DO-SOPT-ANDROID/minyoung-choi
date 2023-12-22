d31f23403d72d5a1eef9733893ad18b7d4bdda64
Choi Min Young <codingmy@naver.com>
2023-12-22 19:19:53 +0900

    [FEAT/#8] singleton 객체 선언

---
+++ app/src/main/java/org/sopt/dosopttemplate/data/dataclass/UserInfo.kt
@@ -0,0 +1,11 @@
+package org.sopt.dosopttemplate.data.dataclass
+
+object UserInfo {
+    var userInfoList = User(
+        nick = "",
+        self_description = "",
+        id = "",
+        pw = "",
+        mbti = "",
+    )
+}
